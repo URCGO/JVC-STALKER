77481f29a0d131e9247f052984136e7338c09e1d
iErcan <ercanfr@gmail.com>
2018-04-29 19:32:37 +0200

    Add files via upload

--- stalker.py
+++ stalker.py
@@ -1,27 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
-
+import os
 print("////////////////////")
 print("/// JV STALKER  ///")
 print("///////////////////")
 print("Outil de stalk")
 auteur = input("Pseudo >>>")
 
+ 
 
-
-url = "http://www.jeuxvideo.com/recherche/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm?search_in_forum=" + auteur + "&type_search_in_forum=auteur_topic"
+url = "http://www.jeuxvideo.com/recherche/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm?search_in_forum=" + str(auteur) + "&type_search_in_forum=auteur_topic"
 page = requests.get(url)
 contents = page.content
 soup = BeautifulSoup(contents, 'html.parser')
-postList = soup.findAll("topic-title");
+postList = soup.findAll("a", { "class" : "lien-jv topic-title" });
 
-
+print(len(postList))
 for i in range(len(postList)):
 	print("["+auteur+"]"+ " : " + postList[i].text)
-
-with open("Stalkresult.txt", "w") as text_file:
-    print("Result: {}".format(postList), file=text_file)
+ 
 
 
 
-    
+
